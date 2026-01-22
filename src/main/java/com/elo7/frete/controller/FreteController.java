b37fe10f7648a3a267d10c0c27ccc58cc1308374
Pradixx <dieguittosilva09@gmail.com>
2026-01-22 19:40:25 -0300

    Controller, foi implementado um unico verbo, o POST, onde salva e responde ao mesmo tempo

---
+++ src/main/java/com/elo7/frete/controller/FreteController.java
@@ -0,0 +1,25 @@
+package com.elo7.frete.controller;
+
+import com.elo7.frete.controller.DTO.FreteResponseDTO;
+import com.elo7.frete.controller.DTO.SalvarProdutoDTO;
+import com.elo7.frete.service.FreteService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/frete")
+@RequiredArgsConstructor
+public class FreteController {
+
+    private final FreteService freteService;
+
+    @PostMapping
+    public List<FreteResponseDTO> criar (@RequestBody SalvarProdutoDTO dto) {
+        return freteService.salvarProduto(dto);
+    }
+}
